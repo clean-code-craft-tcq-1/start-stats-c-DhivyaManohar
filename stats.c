6f8f5e97a485813273f674bd114c8ec919c3006e
DhivyaManohar <77608364+DhivyaManohar@users.noreply.github.com>
2021-01-24 18:06:38 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -2,23 +2,18 @@
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;    
-return s;
-}
-int main()
-{
-    int i;
-    double total retval;
+        int i;
+    double total, retval;
     total = 0;
     for(i=0; i <=setlength; i++)
     {
     total = total + &numberset[i];
     }
     retval = total\setlength;
-    return s;
-    
+    s.average = 0;
+    s.min = 0;
+    s.max = 0;    
+return s;
 }
 
     
